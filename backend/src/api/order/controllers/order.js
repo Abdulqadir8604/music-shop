6cc6a8907d348867b385546f7c380a01a416cb2f
Abdulqadir <abdulqadir.b8604@gmail.com>
2023-06-06 15:29:59 +0530

    u2

--- backend/src/api/order/controllers/order.js
+++ backend/src/api/order/controllers/order.js
@@ -34,7 +34,7 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
 
       const session = await stripe.checkout.sessions.create({
         shipping_address_collection: { allowed_countries: ["IN"] },
-        payment_method_types: ["google_pay", "card"],
+        payment_method_types: ["card"],
         mode: "payment",
         success_url: process.env.CLIENT_URL + `/success`,
         cancel_url: process.env.CLIENT_URL + "/failed",
